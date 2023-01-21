271a1973bb608052689b6ee1fc4e27ca41c54415
RitikaMukherjee <rima.slg1999@gmail.com>
2023-01-21 23:20:55 +0530

    4th commit

--- app/src/main/java/com/example/weconnect/MainActivity.java
+++ app/src/main/java/com/example/weconnect/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.weconnect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.weconnect.Models.Users;
+import com.example.weconnect.databinding.ActivityMainBinding;
+import com.example.weconnect.databinding.ActivitySigninBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+
+        progressDialog=new ProgressDialog(MainActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We're creating your account");
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!binding.username.toString().isEmpty()&&!binding.password.toString().isEmpty()&&!binding.email.toString().isEmpty()&&!binding.college.toString().isEmpty()&&!binding.course.toString().isEmpty()&&!binding.stream.toString().isEmpty()&&!binding.yoa.toString().isEmpty()&&!binding.yop.toString().isEmpty()&&!binding.company.toString().isEmpty()&&!binding.linkedin.toString().isEmpty()){
+                    progressDialog.show();
+                    mAuth.createUserWithEmailAndPassword(binding.email.getText().toString(),binding.password.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+                            if(task.isSuccessful()){
+                                Users user=new Users(binding.username.getText().toString(),binding.email.getText().toString(),binding.password.getText().toString(),binding.college.getText().toString(),binding.course.getText().toString(),binding.stream.getText().toString(),binding.yoa.getText().toString(),binding.yop.getText().toString(),binding.company.getText().toString(),binding.linkedin.getText().toString());
+                                String id= task.getResult().getUser().getUid();
+                                database.getReference().child("Users").child(id).setValue(user);
+                                Toast.makeText(MainActivity.this, "User is signed up successfully", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(MainActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }else {
+                    Toast.makeText(MainActivity.this, "Enter valid credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }

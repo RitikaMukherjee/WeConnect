1d7d724376fa6005544069664188c28e31b9356c
RITIKA MUKHERJEE <60706878+RitikaMukherjee@users.noreply.github.com>
2023-04-03 12:01:46 +0530

    Update MessageModel.java

--- app/src/main/java/com/example/weconnect/Models/MessageModel.java
+++ app/src/main/java/com/example/weconnect/Models/MessageModel.java
@@ -1,36 +1,36 @@
-package com.example.weconnect.Models;
+package com.example.weconnect.Models;//creating packages for ensuring encapsualtion
 
 public class MessageModel {
-    String uId,message,messageId;
-    Long timestamp;
+    Private String uId,message,messageId;// creating private variables to ensure data hiding
+    Private long timestamp;// creating private variables to ensure data hiding
 
-    public MessageModel(String uId, String message) {
+    public MessageModel(String uId, String message) {//constructors created
         this.uId = uId;
         this.message = message;
     }
 
-    public MessageModel(String uId, String message, Long timestamp) {
+    public MessageModel(String uId, String message, Long timestamp) {//constructors created
         this.uId = uId;
         this.message = message;
         this.timestamp = timestamp;
     }
 
-    public MessageModel() {
+    public MessageModel() {// default empty constructor
     }
 
-    public String getuId() {
+    public String getuId() {//creating getters and setters for accessing private variables
         return uId;
     }
 
-    public void setuId(String uId) {
+    public void setuId(String uId) {//creating getters and setters for accessing private variables
         this.uId = uId;
     }
 
-    public String getMessage() {
+    public String getMessage() {//creating getters and setters for accessing private variables
         return message;
     }
 
-    public void setMessage(String message) {
+    public void setMessage(String message) {//creating getters and setters for accessing private variables
         this.message = message;
     }
 
@@ -38,15 +38,15 @@ public class MessageModel {
         return messageId;
     }
 
-    public void setMessageId(String messageId) {
+    public void setMessageId(String messageId) {//creating getters and setters for accessing private variables
         this.messageId = messageId;
     }
 
-    public Long getTimestamp() {
+    public Long getTimestamp() {//creating getters and setters for accessing private variables
         return timestamp;
     }
 
-    public void setTimestamp(Long timestamp) {
+    public void setTimestamp(Long timestamp) {//creating getters and setters for accessing private variables
         this.timestamp = timestamp;
     }
 }
